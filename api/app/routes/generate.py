8a8a9860006f025247520f92a28c47f1ab63579b
imamura.tomoki <nowvilla.physi.311@gmail.com>
2025-06-21 00:39:55 +0900

    ログを追加

--- api/app/routes/generate.py
+++ api/app/routes/generate.py
@@ -14,9 +14,11 @@ index_bp = Blueprint("index", __name__)
 def generate():
     # リクエストを受信
     req = request.get_json()
-    # logging.info("リクエスト: " + json.dumps(req, ensure_ascii=False))
+    logging.info("リクエスト: " + json.dumps(req, ensure_ascii=False))
 
     cardGeneratorService = CardGeneratorService(req)
+    logging.info(cardGeneratorService.prompt_items_level)
+
     promptCreatorService = PromptCreatorService(cardGeneratorService.prompt_items_level)
 
     # プロンプトの生成
